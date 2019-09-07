4c231af49780da59d9776aae4eb64a47fe873728
Egidio CORICA <corica.egidio@gmail.com>
2019-09-07 21:17:06 +0200

    Update survey table ( add name and change wording )

--- database/migrations/2019_08_19_143413_create_surveys_table.php
+++ database/migrations/2019_08_19_143413_create_surveys_table.php
@@ -14,7 +14,8 @@ class CreateSurveysTable extends Migration
     public function up()
     {
         Schema::create('surveys', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
+            $table->string('name');
             $table->timestamps();
         });
     }
