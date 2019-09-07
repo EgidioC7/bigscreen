345616190f2e7a2cbf1d89a0c5bd09f78c5c4331
Egidio CORICA <corica.egidio@gmail.com>
2019-09-07 20:49:41 +0200

    Add model Survey

---
+++ app/Survey.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Survey extends Model
+{
+    public function answer()
+    {
+        return $this->hasMany(Answer::class);
+    }
+    public function usersurvey()
+    {
+        return $this->hasMany(UserSurvey::class);
+    }
+}
