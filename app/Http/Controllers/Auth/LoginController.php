c6773ecd18b384370077d9e7e1f7d4304915660e
Egidio CORICA <corica.egidio@gmail.com>
2019-09-07 21:27:10 +0200

    Add the login controller and modify the redirection URL when accessing the BO

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,9 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected function redirectTo(){
+        return '/administration/survey';
+    } 
 
     /**
      * Create a new controller instance.
