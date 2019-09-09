e5c860308fcd165a69e369161c001f3c7419411c
Egidio CORICA <corica.egidio@gmail.com>
2019-09-09 18:55:54 +0200

    Add partials for the success message with the url survey

---
+++ resources/views/front/partials/flash.blade.php
@@ -0,0 +1,21 @@
+<div class="modal" id="myModal" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Merci d'avoir répondu à notre sondage</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>Voici le lien pour accéder au réponse de votre sondage :</p>
+        @if(Session::has('success'))
+            <p><a href="{{url('/', Session::get('success'))}}">Cliquez ici</a></p>
+        @endif
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+      </div>
+    </div>
+  </div>
+</div>
