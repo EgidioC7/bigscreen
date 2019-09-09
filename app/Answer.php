b1e5c476348aaf9274a031e194824c4f055218aa
Egidio CORICA <corica.egidio@gmail.com>
2019-09-09 18:47:59 +0200

    Add fillable and function to the get question title

--- app/Answer.php
+++ app/Answer.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Answer extends Model
 {
+    protected $fillable = ['value', 'question_id','user_survey_id'];
 
+    public function scopeGetQuestionTitle(){
+
+        return Question::find($this->question_id)->title;
+
+    }
 }
