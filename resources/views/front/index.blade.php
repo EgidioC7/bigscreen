be4f05b7721e6c631679c669262b2097679f4eff
Egidio CORICA <corica.egidio@gmail.com>
2019-09-09 19:00:32 +0200

    Review code (change condition to the display error message and add input hidden to the survey id)

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -2,6 +2,8 @@
 
 @section('content')
 
+@include('front.partials.flash')
+
 <!-- Default form contact -->
 <form class="text-center border border-light p-5" method="POST" action="{{action('FrontController@store')}}">
 
@@ -20,29 +22,28 @@
             @if($question->id === 1)
                 <input type="email" name="question_{{$question->id}}" id="question_{{$question->id}}" class="form-control mb-4" placeholder="E-mail"
                 required value="{{old('question_'.$question->id)}}">
-                @if($errors->has("question_{{$question->id}}")) <span class="error">{{$errors->first("question_".$question->id)}}</span>@endif
+               @if($errors->has('question_'.$question->id)) <span class="error">{{$errors->first("question_".$question->id)}} @endif
             @elseif($question->id === 2)
                 <input type="number" name="question_{{$question->id}}" id="question_{{$question->id}}" class="form-control mb-4"
                 placeholder="{{$question->title}}" value="{{old('question_'.$question->id)}}" required>
-                @if($errors->has("question_{{$question->id}}")) <span class="error">{{$errors->first("question_".$question->id)}}</span>@endif
+                @if($errors->has('question_'.$question->id)) <span class="error">{{$errors->first("question_".$question->id)}}</span>@endif
             @elseif($question->id === 20)
-                <textarea class="form-control" value="{{old('question_'.$question->id)}}" name="question_{{$question->id}}" id="question_{{$question->id}}" rows="3" required></textarea>
-                @if($errors->has("question_{{$question->id}}")) <span class="error">{{$errors->first("question_".$question->id)}}</span>@endif
+                <textarea class="form-control" name="question_{{$question->id}}" id="question_{{$question->id}}" rows="3" required>{{old('question_'.$question->id)}}</textarea>
+                @if($errors->has('question_'.$question->id)) <span class="error">{{$errors->first("question_".$question->id)}}</span>@endif
             @else
             <input type="text" name="question_{{$question->id}}" id="question_{{$question->id}}" class="form-control mb-4"
                 placeholder="{{$question->title}}" required value="{{old('question_'.$question->id)}}">
-                @if($errors->has("question_{{$question->id}}")) <span class="error">{{$errors->first("question_".$question->id)}}</span>@endif
+                @if($errors->has('question_'.$question->id)) <span class="error">{{$errors->first("question_".$question->id)}}</span>@endif
             @endif
         @elseif ($question->question_type === 'selector' )
             <select id="question_{{$question->id}}" name="question_{{$question->id}}" class="browser-default custom-select mb-4"
                 required>
-                <option value="0" {{ is_null(old("question_".$question->id)) ? 'selected' : '' }}>-- Choisir --</option>
+                <option value="0" disabled {{ is_null(old("question_".$question->id)) ? 'selected' : '' }}>-- Choisir --</option>
                 @foreach( unserialize($question->choice) as $choice)
-                <option value="{{$choice}}" {{ (old("question_".$question->id) === $choice ) ? 'selected' : '' }}>{{$choice}}
-                </option>
+                <option value="{{$choice}}" {{ (old("question_".$question->id) === $choice ) ? 'selected' : '' }}>{{$choice}}</option>
                 @endforeach
             </select>
-            @if($errors->has("question_{{$question->id}}")) <span class="error">{{$errors->first("question_".$question->id)}}</span>@endif
+            @if($errors->has('question_'.$question->id)) <span class="error">{{$errors->first("question_".$question->id)}}</span>@endif
         @elseif ($question->question_type === 'number')
             <div class="radio">
                 @for ($i = 1; $i <= 5; $i++) <label class="radio-inline">
@@ -53,7 +54,7 @@
             </div>
         @endif
     </div>
-
+    <input type="hidden" name="survey_id" class="form-control mb-4" value="{{$question->survey_id}}">
     @endforeach
 
     <!-- Send button -->
