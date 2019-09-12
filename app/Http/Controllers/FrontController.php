ac55a7666ec9170b27a2ed9596ffdd37dd6fb7ca
Egidio CORICA <corica.egidio@gmail.com>
2019-09-12 09:50:17 +0200

    Update FronController (change methode)

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,21 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Answer;
 use App\Question;
 use App\UserSurvey;
-use App\Answer;
+use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
 class FrontController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $questions = Question::all();
 
         return view('front.index', ['questions' => $questions]);
     }
 
-     /**
+    /**
      * Store a newly created resource in storage.
      *
      * @param \Illuminate\Http\Request $request
@@ -47,50 +48,42 @@ class FrontController extends Controller
             'question_20' => 'required',
         ]);
 
-      $email_user = $request->question_1;
-      $survey_id = $request->survey_id;
-      $user_exist = UserSurvey::where('email', $email_user )->where('survey_id',$survey_id)->first();
-      if( is_null($user_exist) ){
+        if (UserSurvey::whereEmail($request->question_1)->exists()) {
+            return redirect()->back()
+                ->withInput($request->all())
+                ->withErrors(['question_1' => 'Il existe déjà un utilisateur avec la même adresse mail']);
+        }
+
+        $email_user = $request->question_1;
+        $survey_id = $request->survey_id;
         $new_user = new UserSurvey();
         $new_user->email = $email_user;
-        $new_user->link =  Str::uuid($email_user)->toString();
+        $new_user->link = Str::uuid($email_user)->toString();
         $new_user->survey_id = $survey_id;
         $new_user->save();
-      }
 
-      foreach($request->all() as $key => $value){
-          if($key === '_token' || $key === 'survey_id' ){
-              continue;
-          }
-          else{
+        foreach ($request->all() as $key => $value) {
+            if ($key === '_token' || $key === 'survey_id') {
+                continue;
+            } else {
                 $key = str_replace('question_', '', $key);
-                if(!empty($user_exist)){
-                    $exist_answer = Answer::where('user_survey_id', $user_exist->id)->where('question_id',$key)->first();
-                    $exist_answer->update(['value' => $value]);
-                }
-                else{
-                    Answer::create([
-                        'question_id' => $key,
-                        'value' => $value,
-                        'user_survey_id' => $new_user->id,
-                    ]);
-                }
-          }
-      }
+                Answer::create([
+                    'question_id' => $key,
+                    'value' => $value,
+                    'user_survey_id' => $new_user->id,
+                ]);
 
-      if(!is_null($user_exist)){
-        return redirect('/')->with('success', $user_exist->link);
-      }
-      else{
+            }
+        }
         return redirect('/')->with('success', $new_user->link);
-      }
     }
 
-    public function anwser($link){
+    public function anwser($link)
+    {
 
         $user = UserSurvey::where('link', $link)->first();
 
-        if(empty($user)){
+        if (empty($user)) {
             return redirect('/');
         }
 
