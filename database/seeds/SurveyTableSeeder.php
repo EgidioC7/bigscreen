aa557ab3b2b65266ee5f47512e8a078679b1968f
Egidio CORICA <corica.egidio@gmail.com>
2019-09-12 15:15:09 +0200

    Add seeder to the survey table

---
+++ database/seeds/SurveyTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SurveyTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('surveys')->insert(
+            [
+                'name' => 'first',
+            ],
+            [
+                'name' => 'second',
+            ]
+        );
+    }
+}
