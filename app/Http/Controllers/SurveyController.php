f52b9c4d95c787d73aa92e9d1f6b496f1ad3019e
Egidio CORICA <corica.egidio@gmail.com>
2019-09-13 17:38:57 +0200

    Add methode for the pie chart

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -22,31 +22,18 @@ class SurveyController extends Controller
 
        $question_chart_10 = $this->pieChart(10);
        $pie_chart_10 = $question_chart_10['chart'];
+
+       $question_chart_11 = $this->radarChart(11);
+       $pie_chart_11 = $question_chart_11['chart'];
       
      $questions_title = [
            '1' => $question_chart_6['title'], 
            '2' => $question_chart_7['title'], 
-           '3' => $question_chart_10['title'] 
+           '3' => $question_chart_10['title'],
+           '4' => $question_chart_11['title'],
         ];
-
-      //  $question_chart_10 = $this->radarChart(10);
-
-        /*$chart_data =  [
-            (   object)[
-                    'chart' => compact('pie_chart_6'), 
-                    'question_title' => $question_chart_6['title'] 
-                ],
-                (object)  [
-                    'chart' => compact('pie_chart_7'), 
-                    'question_title' => $question_chart_7['title'] 
-                ],
-                (object) [
-                    'chart' => compact('pie_chart_10'), 
-                    'question_title' => $question_chart_10['title'] 
-                ],
-            ];*/
     
-    return view('back.home.index',["charts" => compact('pie_chart_6','pie_chart_7','pie_chart_10'), 'questions_title' => $questions_title ]);
+         return view('back.home.index',["charts" => compact('pie_chart_6','pie_chart_7','pie_chart_10', 'pie_chart_11'), 'questions_title' => $questions_title ]);
     }
 
     public function pieChart(int $question_id){
@@ -92,9 +79,11 @@ class SurveyController extends Controller
         $question = Question::find($question_id);
         $answers = Answer::where('question_id', $question_id)->get();
         
-        
         $pie_chart = [];
-        $pie_chart['label'] = unserialize($question->choice);
+
+        for($i=1; $i<=5; $i++){
+            $pie_chart['label'][] = $i;
+        }
        
         foreach($pie_chart['label'] as $label){
             $pie_chart['data'][$label] = 0;
@@ -106,12 +95,12 @@ class SurveyController extends Controller
                 $pie_chart['data'][$answer->value] += 1;
             }
         }
-        
+
         $chartjs = app()->chartjs
         ->name("question_$question_id")
         ->type('radar')
         ->size(['width' => 750, 'height' => 700])
-        ->labels()
+        ->labels($pie_chart['label'])
         ->datasets([
             [
                 "label" => "My First dataset",
@@ -121,22 +110,11 @@ class SurveyController extends Controller
                 "pointBackgroundColor" => "rgba(38, 185, 154, 0.7)",
                 "pointHoverBackgroundColor" => "#fff",
                 "pointHoverBorderColor" => "rgba(220,220,220,1)",
-                'data' => [65, 59, 80, 81, 56, 55, 40],
-            ],
-            [
-                "label" => "My Second dataset",
-                'backgroundColor' => "rgba(38, 185, 154, 0.31)",
-                'borderColor' => "rgba(38, 185, 154, 0.7)",
-                "pointBorderColor" => "rgba(38, 185, 154, 0.7)",
-                "pointBackgroundColor" => "rgba(38, 185, 154, 0.7)",
-                "pointHoverBackgroundColor" => "#fff",
-                "pointHoverBorderColor" => "rgba(220,220,220,1)",
-                'data' => [12, 33, 44, 44, 55, 23, 40],
+                'data' => array_values($pie_chart['data'])
             ]
         ])
         ->options([]);
 
-return view('example', compact('chartjs'));
         $all_chart_data = ['chart' => $chartjs, 'title' => $question->title];
 
         return $all_chart_data;
