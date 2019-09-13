1fe104d55ce10a5109ba0669eacfaaa9a232ba78
Egidio CORICA <corica.egidio@gmail.com>
2019-09-13 17:47:54 +0200

    Indent line

--- resources/views/back/home/index.blade.php
+++ resources/views/back/home/index.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
 <div class="row">
-@foreach($charts as $chart )
-<div class="col-sm-6">
-<p class="text-white">{{$questions_title[$loop->iteration]}}</p>
-    {!! $chart->render() !!}
-</div>
-@endforeach
+    @foreach($charts as $chart )
+    <div class="col-sm-6">
+        <p class="text-white">{{$questions_title[$loop->iteration]}}</p>
+        {!! $chart->render() !!}
+    </div>
+    @endforeach
 </div>
 
 @endsection
