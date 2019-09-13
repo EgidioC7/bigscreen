422a279553126d5ee7d98f32f315f110e6840c13
Egidio CORICA <corica.egidio@gmail.com>
2019-09-13 17:47:07 +0200

    Reformat code and add date time

--- resources/views/front/show.blade.php
+++ resources/views/front/show.blade.php
@@ -2,25 +2,23 @@
 
 @section('content')
 
-    <p class="h1 mb-4">Contact us</p>
+<p>Vous trouverez ci-dessous les réponses que vous avez apportées à notre sondage le {{$user_date}} </p>
 
-    <p>Voici les réponses de votre formulaire</p>
-
-    <table class="table table-bordered">
+<table class="table table-bordered">
     <thead>
         <tr>
-        <th scope="col">Numéro de la question</th>
-        <th scope="col">Corps de la question</th>
-        <th scope="col">Réponses</th>
+            <th scope="col">Numéro de la question</th>
+            <th scope="col">Corps de la question</th>
+            <th scope="col">Réponses</th>
         </tr>
     </thead>
     <tbody>
-    @foreach($answers as $answer )
-            <tr>
-                <th scope="row">{{$answer->question_id}}</th>
-                <td>{{$answer->getQuestionTitle()}}</td>
-                <td>{{$answer->value}}</td>
-            </tr>
+        @foreach($answers as $answer )
+        <tr>
+            <th scope="row">{{$answer->question_id}}</th>
+            <td>{{$answer->getQuestionTitle()}}</td>
+            <td>{{$answer->value}}</td>
+        </tr>
         @endforeach
     </tbody>
 </table>
