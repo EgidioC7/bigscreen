850952471f0b5ed1a3644a8e49df2c9bbf667fa1
Egidio CORICA <corica.egidio@gmail.com>
2019-09-13 23:16:40 +0200

    Add menu

---
+++ resources/views/partials/menu.blade.php
@@ -0,0 +1,6 @@
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-light bg-dark shadow fixed-top">
+  <div class="container">
+    <a class="navbar-brand" href="{{url('/')}}"><img src="{{asset('images/bigscreen_logo.png')}}"></a>
+  </div>
+</nav>
