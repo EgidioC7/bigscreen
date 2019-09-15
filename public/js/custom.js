f72c3ae6cde9a8b67c2e4c164a48759462dc425a
Egidio CORICA <corica.egidio@gmail.com>
2019-09-15 16:57:14 +0200

    Remove unused event js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,17 +1,7 @@
-$(function() {
-    $('#question_8').change(function() {
-        $this = $(this);
-        if($this.val() == 'Autre'){
-            $this.after('<input placeholder="Autre :" type="text"id="input_8" class="form-control mb-4" name="question_8"/>');
-        }
-        else{
-            $('#input_8').remove()
-        }
+$(function () {
+    // Sidebar toggle behavior
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar, #content').toggleClass('active');
     });
 
-      // Sidebar toggle behavior
-  $('#sidebarCollapse').on('click', function() {
-    $('#sidebar, #content').toggleClass('active');
-  });
-
- });
+});
