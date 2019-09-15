75a737812734bae1dd9f1e99545fcc1730e29d85
Egidio CORICA <corica.egidio@gmail.com>
2019-09-15 17:50:55 +0200

    Fix missing span tag and reformat code

--- resources/views/front/survey.blade.php
+++ resources/views/front/survey.blade.php
@@ -21,50 +21,51 @@
                                class="form-control mb-4" placeholder="E-mail"
                                required value="{{old('question_'.$question->id)}}">
                         @if($errors->has('question_'.$question->id)) <span
-                                class="error">{{$errors->first("question_".$question->id)}} @endif
-                            @elseif($question->id === 2)
-                                <input type="number" name="question_{{$question->id}}" id="question_{{$question->id}}"
-                                       class="form-control mb-4"
-                                       placeholder="{{$question->title}}" value="{{old('question_'.$question->id)}}"
-                                       required>
-                                @if($errors->has('question_'.$question->id)) <span
-                                        class="error">{{$errors->first("question_".$question->id)}}</span>@endif
-                            @elseif($question->id === 20)
-                                <textarea class="form-control" name="question_{{$question->id}}"
-                                          id="question_{{$question->id}}" rows="3"
-                                          required>{{old('question_'.$question->id)}}</textarea>
-                                @if($errors->has('question_'.$question->id)) <span
-                                        class="error">{{$errors->first("question_".$question->id)}}</span>@endif
-                            @else
-                                <input type="text" name="question_{{$question->id}}" id="question_{{$question->id}}"
-                                       class="form-control mb-4"
-                                       placeholder="{{$question->title}}" required
-                                       value="{{old('question_'.$question->id)}}">
-                                @if($errors->has('question_'.$question->id)) <span
-                                        class="error">{{$errors->first("question_".$question->id)}}</span>@endif
-                            @endif
-                            @elseif ($question->question_type === 'A' )
-                                <select id="question_{{$question->id}}" name="question_{{$question->id}}"
-                                        class="browser-default custom-select mb-4"
-                                        required>
-                <option value="0"
-                        disabled {{ is_null(old("question_".$question->id)) ? 'selected' : '' }}>-- Choisir --</option>
-                @foreach( unserialize($question->choice) as $choice)
-                                        <option value="{{$choice}}" {{ (old("question_".$question->id) === $choice ) ? 'selected' : '' }}>{{$choice}}</option>
-                                    @endforeach
-            </select>
-                                @if($errors->has('question_'.$question->id)) <span
-                                        class="error">{{$errors->first("question_".$question->id)}}</span>@endif
-                            @elseif ($question->question_type === 'C')
-                                <div class="radio">
-                @for ($i = 1; $i <= 5; $i++) <label class="radio-inline">
-                    <input type="radio" name="question_{{$question->id}}" value="{{$i}}" {{ ( old("question_".$question->id) != $i )
+                                class="error">{{$errors->first("question_".$question->id)}}</span> @endif
+                    @elseif($question->id === 2)
+                        <input type="number" name="question_{{$question->id}}" id="question_{{$question->id}}"
+                               class="form-control mb-4"
+                               placeholder="{{$question->title}}" value="{{old('question_'.$question->id)}}"
+                               required>
+                        @if($errors->has('question_'.$question->id)) <span
+                                class="error">{{$errors->first("question_".$question->id)}}</span>@endif
+                    @elseif($question->id === 20)
+                        <textarea class="form-control" name="question_{{$question->id}}"
+                                  id="question_{{$question->id}}" rows="3"
+                                  required>{{old('question_'.$question->id)}}</textarea>
+                        @if($errors->has('question_'.$question->id)) <span
+                                class="error">{{$errors->first("question_".$question->id)}}</span>@endif
+                    @else
+                        <input type="text" name="question_{{$question->id}}" id="question_{{$question->id}}"
+                               class="form-control mb-4"
+                               placeholder="{{$question->title}}" required
+                               value="{{old('question_'.$question->id)}}">
+                        @if($errors->has('question_'.$question->id)) <span
+                                class="error">{{$errors->first("question_".$question->id)}}</span>@endif
+                    @endif
+                @elseif ($question->question_type === 'A' )
+                    <select id="question_{{$question->id}}" name="question_{{$question->id}}"
+                            class="browser-default custom-select mb-4"
+                            required>
+                        <option value="0"
+                                disabled {{ is_null(old("question_".$question->id)) ? 'selected' : '' }}>-- Choisir --
+                        </option>
+                        @foreach( unserialize($question->choice) as $choice)
+                            <option value="{{$choice}}" {{ (old("question_".$question->id) === $choice ) ? 'selected' : '' }}>{{$choice}}</option>
+                        @endforeach
+                    </select>
+                    @if($errors->has('question_'.$question->id)) <span
+                            class="error">{{$errors->first("question_".$question->id)}}</span>@endif
+                @elseif ($question->question_type === 'C')
+                    <div class="radio">
+                        @for ($i = 1; $i <= 5; $i++) <label class="radio-inline">
+                            <input type="radio" name="question_{{$question->id}}" value="{{$i}}" {{ ( old("question_".$question->id) != $i )
                         ? ( ($i === 1) ? 'checked' : '' ) : 'checked' }} required>{{$i}}<span class="outside"><span
-                                                    class="inside"></span></span>
-                    </label>
-                                    @endfor
-            </div>
-                        @endif
+                                        class="inside"></span></span>
+                        </label>
+                        @endfor
+                    </div>
+                @endif
             </div>
 
         @endforeach
