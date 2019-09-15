0ef4a54da022b7d7178ff596acff38cc5f02f77b
Egidio CORICA <corica.egidio@gmail.com>
2019-09-15 14:54:26 +0200

    Add relation model between question and answer, Remove unused function

--- app/Answer.php
+++ app/Answer.php
@@ -7,12 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Answer extends Model
 {
     protected $fillable = ['value', 'question_id','user_survey_id'];
-
-    public function scopeGetQuestionTitle(){
-
-        return Question::find($this->question_id)->title;
-    }
+    
     public function user(){
         return $this->belongsTo(UserSurvey::class);
+    }
+    public function question(){
+        return $this->belongsTo(Question::class);
     }
 }
