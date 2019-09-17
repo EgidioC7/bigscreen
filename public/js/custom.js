fc1b0730dd4123a3733b3d5e60a3d0691c82dd54
Egidio CORICA <corica.egidio@gmail.com>
2019-09-17 14:06:11 +0200

    Add custom js for the responsive menu

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,7 +1,20 @@
 $(function () {
     // Sidebar toggle behavior
     $('#sidebarCollapse').on('click', function () {
-        $('#sidebar, #content').toggleClass('active');
+    	$('#sidebar, #content').toggleClass('active');
+    });
+    
+    $('.first-button').on('click', function () {
+
+    	$('.animated-icon1').toggleClass('open');
+    });
+    $('.second-button').on('click', function () {
+
+    	$('.animated-icon2').toggleClass('open');
+    });
+    $('.third-button').on('click', function () {
+
+    	$('.animated-icon3').toggleClass('open');
     });
 
 });
