eaa8c3666acc04deb5810348803c1ed1dc02d1d4
Egidio CORICA <corica.egidio@gmail.com>
2019-09-17 15:25:53 +0200

    Add class to the labels

--- resources/views/back/home/index.blade.php
+++ resources/views/back/home/index.blade.php
@@ -4,11 +4,11 @@
     <div class="row">
         @foreach($charts as $chart )
             <div class="col-xl-6 col-lg-12">
-                <div class="card ">
-                    <div class="card-header white">
+                <div class="card card-white">
+                    <div class="card-header">
                         <h5 class="card-title">{{$questions_title[$loop->iteration]}}</h5>
                     </div>
-                    <div class="card-body white">
+                    <div class="card-body">
                         {!! $chart->render() !!}
                     </div>
                 </div>
